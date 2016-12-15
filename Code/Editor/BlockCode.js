9b55bc8c9729c622ffb487780ab531d54d466284
K <wlzla000@naver.com>
2016-12-15 20:36:20 +0900

    Improved the code, added some WebGL blocks.
    The new blocks:
    - `set_clear_code`
    - `create_program`

--- Code/Editor/BlockCode.js
+++ Code/Editor/BlockCode.js
@@ -1,40 +1,111 @@
+"use strict";
 
-'use strict';
+let Code = {};
+Code.workspace = Blockly.inject("blocklyDiv", {
+		media: "../../media/",
+		toolbox: document.getElementById("toolbox"),
+		grid: {
+				spacing: 40,
+				length: 3,
+				colour: "#CCC",
+				snap: true
+			},
+		trashcan: true,
+		zoom: {
+				controls: true,
+				wheel: true
+			}
+	});
 
-var Code = {};
-Code.workspace = Blockly.inject('blocklyDiv',
-        {media: '../../media/',
-         toolbox: document.getElementById('toolbox'),
-		 grid:
-         {spacing: 40,
-          length: 	3,
-          colour: '#ccc',
-          snap: true},
-     trashcan: true,
-		   zoom:
-        {controls: true,
-          wheel: true}
-		});
-  
+Code.blocklyArea = document.getElementById("blocklyArea");
+Code.blocklyDiv = document.getElementById("blocklyDiv");
+Code.onresize = function() {
+		let element = Code.blocklyArea;
+		let x = 0;
+		let y = 0;
+		do {
+			x += element.offsetLeft;
+			y += element.offsetTop;
+			element = element.offsetParent;
+		} while(element);
+		Code.blocklyDiv.style.left = x + 'px';
+		Code.blocklyDiv.style.top = y + 'px';
+		Code.blocklyDiv.style.width = Code.blocklyArea.offsetWidth + 'px';
+		Code.blocklyDiv.style.height = Code.blocklyArea.offsetHeight + 'px';
+		Blockly.svgResize(Code.workspace);
+	};
+window.addEventListener("resize", Code.onresize, false);
+Code.onresize();
 
 
-  Code.blocklyArea = document.getElementById('blocklyArea');
-  Code.blocklyDiv = document.getElementById('blocklyDiv');
-  Code.onresize = function() {
-    var element = Code.blocklyArea;
-    var x = 0;
-    var y = 0;
-    do {
-      y += element.offsetTop;
-	  x += element.offsetLeft;
-      element = element.offsetParent;
-    } while (element);
-    Code.blocklyDiv.style.left = x + 'px';
-    Code.blocklyDiv.style.top = y + 'px';
-    Code.blocklyDiv.style.width = Code.blocklyArea.offsetWidth + 'px';
-    Code.blocklyDiv.style.height = Code.blocklyArea.offsetHeight + 'px';
-	  Blockly.svgResize(Code.workspace);
-  };
-  window.addEventListener('resize', Code.onresize, false);
-  Code.onresize();
-	
+
+Code.workspace.addChangeListener(event => {
+		document.getElementById("Info").textContent
+		= Blockly.JavaScript.workspaceToCode(Code.workspace);
+	});
+
+[ // blocks
+	{
+		"type": "create_program",
+		"message0": "Create program %1",
+		"args0": [
+			{
+				"type": "field_variable",
+				"name": "gl_program",
+				"variable": "variable"
+			}
+		],
+		"inputsInline": true,
+		"previousStatement": null,
+		"nextStatement": null,
+		"colour": 0,
+		"tooltip": "",
+		"helpUrl": "http://www.example.com/"
+	},
+	{
+		"type": "set_clear_color",
+		"message0": "Set the clear color %1 ( alpha:  %2 )",
+		"args0": [
+			{
+				"type": "field_colour",
+				"name": "clear_color",
+				"colour": "#000000"
+			},
+			{
+				"type": "field_number",
+				"name": "alpha",
+				"value": 1,
+				"min": 0,
+				"max": 1,
+				"precision": 0.000001
+			}
+		],
+		"inputsInline": true,
+		"previousStatement": null,
+		"nextStatement": null,
+		"colour": 0,
+		"tooltip": "",
+		"helpUrl": "http://www.example.com/"
+	}
+].forEach(blockInfo => {
+		Blockly.Blocks[blockInfo.type] = {init: function() {
+				this.jsonInit(blockInfo);
+			}};
+	});
+
+const hexColorCodeToFloatVector = colorHex => colorHex
+	.substr(1).match(/.{2}/g).map(rgbHex => parseInt(rgbHex, 16) / 255);
+Object.assign(Blockly.JavaScript, {
+		"create_program": function(block) {
+			const variable_gl_program = Blockly.JavaScript.variableDB_
+				.getName(block.getFieldValue("gl_program"), Blockly.Variables.NAME_TYPE);
+
+			return `${variable_gl_program} = gl.createProgram();\n`;
+		},
+		"set_clear_color": function(block) {
+			const colour_clear_color = block.getFieldValue("clear_color"); // #xxxxxx
+			const number_alpha = block.getFieldValue("alpha");
+
+			return `gl.clearColor(${hexColorCodeToFloatVector(colour_clear_color).join(", ")}, ${number_alpha});\n`;
+		}
+	});
--- Code/Editor/WebGL.js
+++ Code/Editor/WebGL.js
@@ -1,37 +1,41 @@
-var gl; 
-var ShaderProgram;
-function start() {
-  var canvas = document.getElementById("glcanvas");
+let gl;
+let shaderProgram;
 
-  gl = initWebGL(canvas);      
-  initShader();
-  
-  if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);                      
-    gl.enable(gl.DEPTH_TEST);                              
-    gl.depthFunc(gl.LEQUAL);                                
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      
-  }
-} 
+window.addEventListener("load", function start() {
+		const canvas = document.getElementById("glcanvas");
+
+		gl = initWebGL(canvas);      
+		initShader();
+
+		if(gl)
+		{
+			gl.clearColor(0.0, 0.0, 0.0, 1.0);
+			gl.enable(gl.DEPTH_TEST);
+			gl.depthFunc(gl.LEQUAL);
+			gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      
+		}
+	});
 
 function initShader()
 {
-	ShaderProgram = gl.createProgram();
+	shaderProgram = gl.createProgram();
 }
 
-function initWebGL(canvas) {
-  gl = null;
-  
-  try {
-    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-  }
-  catch(e) {}
-  
-  if (!gl) {
-    alert("Unable to initialize WebGL. Your browser may not support it.");
-    gl = null;
-  }
-  
-  return gl;
-}	
+function initWebGL(canvas)
+{
+	gl = null;
 
+	try
+	{
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+	}
+	catch(e) {}
+
+	if(!gl)
+	{
+		alert("Unable to initialize WebGL. Your browser may not support it.");
+		gl = null;
+	}
+
+	return gl;
+}
