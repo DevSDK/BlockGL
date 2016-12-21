3eb29a53157336453d361dfb9906443bb3ff6d56
K <wlzla000@naver.com>
2016-12-21 23:51:59 +0900

    More BLOOOOCKS!!!!

--- Code/Editor/BlockCode.js
+++ Code/Editor/BlockCode.js
@@ -1,4 +1,4 @@
-"use strict";
+﻿"use strict";
 
 const infoDiv = document.getElementById("info");
 
@@ -129,27 +129,8 @@ window.addEventListener("load", _ => {
 
 		Blockly.Xml.domToWorkspace(
 			Blockly.Xml.textToDom(`
-				<xml>
-					<block type="procedures_defnoreturn" x="50" y="100">
-						<field name="NAME">initialize</field>
-						<comment pinned="false" h="50" w="200">To initialize the stuff</comment>
-					</block>
-					<block type="procedures_defnoreturn" x="250" y="100">
-						<field name="NAME">render</field>
-						<comment pinned="false" h="50" w="200">A callback for the rendering loop</comment>
-						<statement name="STACK">
-							<block type="set_clear_color" x="50" y="50">
-								<field name="clear_color">#ffcc00</field>
-								<field name="alpha">1</field>
-								<next>
-									<block type="clear_buffer">
-										<field name="buffer">COLOR_BUFFER_BIT</field>
-									</block>
-								</next>
-							</block>
-						</statement>
-					</block>
-				</xml>`),
+<xml><block type="procedures_defnoreturn" x="20" y="100"><field name="NAME">initialize</field><comment pinned="false" h="50" w="200">To initialize the stuff</comment></block><block type="procedures_defnoreturn" x="220" y="260"><field name="NAME">render</field><comment pinned="false" h="50" w="200">A callback for the rendering loop</comment><statement name="STACK"><block type="set_clear_color"><field name="clear_color">#ffcc00</field><field name="alpha">1</field><next><block type="clear_buffer"><field name="buffer">COLOR_BUFFER_BIT</field><next><block type="create_buffer"><value name="buffer_variable"><block type="variables_get"><field name="VAR">vertex_buffer</field></block></value><next><block type="bind_buffer"><field name="buffer">vertex_buffer</field><field name="target">ARRAY_BUFFER</field><next><block type="buffer_data"><field name="target">ARRAY_BUFFER</field><field name="array_type">Float32Array</field><field name="usage">STATIC_DRAW</field><statement name="data"><block type="vertex"><field name="x">-0.5</field><field name="y">0.5</field><field name="z">0</field><next><block type="vertex"><field name="x">-0.5</field><field name="y">-0.5</field><field name="z">0</field><next><block type="vertex"><field name="x">0.5</field><field name="y">-0.5</field><field name="z">0</field></block></next></block></next></block></statement></block></next></block></next></block></next></block></next></block></statement></block></xml>
+`),
 			BlockGL.workspace
 		);
 	});
@@ -262,7 +243,214 @@ Blockly.BlockSvg.START_HAT = true;
 		"colour": 0,
 		"tooltip": "",
 		"helpUrl": "http://www.example.com/"
-	}
+	},
+{
+  "type": "create_buffer",
+  "message0": "Create a buffer %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "buffer_variable"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+},
+{
+  "type": "bind_buffer",
+  "message0": "Bind a buffer %1 to %2",
+  "args0": [
+    {
+      "type": "field_variable",
+      "name": "buffer",
+      "variable": "buffer"
+    },
+    {
+      "type": "field_dropdown",
+      "name": "target",
+      "options": [
+        [
+          "ARRAY_BUFFER",
+          "ARRAY_BUFFER"
+        ],
+        [
+          "ELEMENT_ARRAY_BUFFER",
+          "ELEMENT_ARRAY_BUFFER"
+        ],
+        [
+          "COPY_READ_BUFFER",
+          "COPY_READ_BUFFER"
+        ],
+        [
+          "COPY_WRITE_BUFFER",
+          "COPY_WRITE_BUFFER"
+        ],
+        [
+          "TRANSFORM_FEEDBACK_BUFFER",
+          "TRANSFORM_FEEDBACK_BUFFER"
+        ],
+        [
+          "UNIFORM_BUFFER",
+          "UNIFORM_BUFFER"
+        ],
+        [
+          "PIXEL_PACK_BUFFER",
+          "PIXEL_PACK_BUFFER"
+        ],
+        [
+          "PIXEL_UNPACK_BUFFER",
+          "PIXEL_UNPACK_BUFFER"
+        ]
+      ]
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+},
+{
+  "type": "buffer_data",
+  "message0": "Initialize %1 with %2 %3 %4 for %5",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "target",
+      "options": [
+        [
+          "ARRAY_BUFFER",
+          "ARRAY_BUFFER"
+        ],
+        [
+          "ELEMENT_ARRAY_BUFFER",
+          "ELEMENT_ARRAY_BUFFER"
+        ],
+        [
+          "COPY_READ_BUFFER",
+          "COPY_READ_BUFFER"
+        ],
+        [
+          "COPY_WRITE_BUFFER",
+          "COPY_WRITE_BUFFER"
+        ],
+        [
+          "TRANSFORM_FEEDBACK_BUFFER",
+          "TRANSFORM_FEEDBACK_BUFFER"
+        ],
+        [
+          "UNIFORM_BUFFER",
+          "UNIFORM_BUFFER"
+        ],
+        [
+          "PIXEL_PACK_BUFFER",
+          "PIXEL_PACK_BUFFER"
+        ],
+        [
+          "PIXEL_UNPACK_BUFFER",
+          "PIXEL_UNPACK_BUFFER"
+        ]
+      ]
+    },
+    {
+      "type": "field_dropdown",
+      "name": "array_type",
+      "options": [
+        [
+          "Uint16Array",
+          "Uint16Array"
+        ],
+        [
+          "Float32Array",
+          "Float32Array"
+        ]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "data"
+    },
+    {
+      "type": "field_dropdown",
+      "name": "usage",
+      "options": [
+        [
+          "STATIC_DRAW",
+          "STATIC_DRAW"
+        ],
+        [
+          "DYNAMIC_DRAW",
+          "DYNAMIC_DRAW"
+        ],
+        [
+          "STREAM_DRAW",
+          "STREAM_DRAW"
+        ],
+        [
+          "STATIC_READ",
+          "STATIC_READ"
+        ],
+        [
+          "DYNAMIC_READ",
+          "DYNAMIC_READ"
+        ],
+        [
+          "STREAM_READ",
+          "STREAM_READ"
+        ],
+        [
+          "STATIC_COPY",
+          "STATIC_COPY"
+        ],
+        [
+          "DYNAMIC_COPY",
+          "DYNAMIC_COPY"
+        ],
+        [
+          "STREAM_COPY",
+          "STREAM_COPY"
+        ]
+      ]
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+},
+{
+  "type": "vertex",
+  "message0": "x %1 | y %2 | z %3",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "x",
+      "value": 0
+    },
+    {
+      "type": "field_number",
+      "name": "y",
+      "value": 0
+    },
+    {
+      "type": "field_number",
+      "name": "z",
+      "value": 0
+    }
+  ],
+  "previousStatement": "Array",
+  "nextStatement": "Array",
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": "http://www.example.com/"
+}
 ].forEach(blockInfo => {
 		Blockly.Blocks[blockInfo.type] = {init: function() {
 				this.jsonInit(blockInfo);
@@ -298,5 +486,33 @@ Object.assign(Blockly.JavaScript, {
 			const dropdown_buffer = block.getFieldValue('buffer');
 
 			return `gl.clear(gl.${dropdown_buffer});\n`;
+		},
+		"create_buffer": function(block) {
+			let value_buffer_variable = Blockly.JavaScript.valueToCode(block, "buffer_variable", Blockly.JavaScript.ORDER_ATOMIC);
+
+			return `${value_buffer_variable} = gl.createBuffer();\n`;
+		},
+		"bind_buffer": function(block) {
+			let variable_buffer = Blockly.JavaScript.variableDB_.getName(block.getFieldValue("buffer"), Blockly.Variables.NAME_TYPE);
+			let dropdown_target = block.getFieldValue("target");
+
+			return `gl.bindBuffer(gl.${dropdown_target}, ${variable_buffer ? variable_buffer : null});\n`;
+		},
+		"buffer_data": function(block) {
+			let dropdown_target = block.getFieldValue("target");
+			let dropdown_array_type = block.getFieldValue("array_type");
+			let statements_data = Blockly.JavaScript.statementToCode(block, "data");
+			let dropdown_usage = block.getFieldValue("usage");
+
+			return `gl.bufferData(gl.${dropdown_target}, `
+				+ `new ${dropdown_array_type}([${statements_data}].filter(e => e !== undefined)), `
+				+ `gl.${dropdown_usage});\n`;
+		},
+		"vertex": function(block) {
+			var number_x = block.getFieldValue('x');
+			var number_y = block.getFieldValue('y');
+			var number_z = block.getFieldValue('z');
+
+			return `${number_x}, ${number_y}, ${number_z}, `;
 		}
 	});
